329986ad760094398190c78266920c6ae2258a50
Ewan Sheldon <ewansheldon@gmail.com>
2016-09-02 10:46:48 +0100

    global variables replaced

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,22 @@
 require 'game'
 describe Game do
 
-  subject(:game) {described_class.new}
-  let(:player_one) { double :player }
-  let(:player_one) { double :player }
+  subject(:game) {described_class.new(player_1, player_2)}
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
 
 
   describe '#attack' do
     it 'attacks player_one' do
-      expect(player_one).to receive(:receive_damage)
-      game.attack(player_one)
+      expect(player_1).to receive(:receive_damage)
+      game.attack(player_1)
+    end
+  end
+
+  describe '#initialize' do
+    it 'saves both player names' do
+      expect(game.player_1).to eq player_1
+      expect(game.player_2).to eq player_2
     end
   end
 end
